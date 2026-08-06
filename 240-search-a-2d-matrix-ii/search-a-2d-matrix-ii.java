178d98c7f4e64c7d2ee764b3ccd03f3230bca9a8
dev singh <devsinghrajwar16@gmail.com>
2026-08-07 01:28:59 +0530

    Time: 3 ms (88.44%) | Memory: 48.5 MB (10.17%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,46 +1,22 @@
 class Solution {
-    public boolean find(int []arr,int k)
-    {
-        boolean ans=false;
-        int n=arr.length;
-        if(k>arr[n-1])
-        {
-            return ans;
-        }
-        int h=n-1;
-        int l=0;
-        while(l<=h)
-        {
-            int mid=(h+l)/2;
-            if(arr[mid]==k)
-            {
-                ans=true;
-                break;
-            }
-            else if(arr[mid]<k)
-            {
-              l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }
-        }
-        return ans;
-    }
-    public boolean searchMatrix(int[][] mat, int target) {
-         int rows = mat.length;          
-        boolean ans=false;  
-        for(int i=0;i<rows;i++)
-        {
-                 if(find(mat[i],target))
-                 {
-                    ans=true;
-                 }
-                 else{
-                    continue;
-                 }
-        }
-        return ans;
+    public boolean searchMatrix(int[][] matrix, int target) {
+         int rows=0;
+         int n=matrix.length;
+         int cols=matrix[0].length-1;
+         while(rows<n && cols>=0)
+         {
+           if(matrix[rows][cols]==target)
+           {
+            return true;
+           }
+           else if(matrix[rows][cols]<target)
+           {
+             rows++;
+           }
+           else{
+            cols--;
+           }
+         }
+         return false;
     }
 }
-
