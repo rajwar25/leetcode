aa137a3079979861616b33d4e2fa00ea71ed414c
dev singh <devsinghrajwar16@gmail.com>
2026-08-11 23:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (44.13%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -10,5 +10,6 @@ class Solution {
     public void deleteNode(ListNode node) {
         node.val=node.next.val;
         node.next=node.next.next;
+        //jo node hai lets say 5 hmne voh bheja, 5 waale node mei hmne uske next node value daaldi and then hmne us node ko uske aage point krwah diya, a kind of a cheat way!!
     }
 }
